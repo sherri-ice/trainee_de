75b1fd8d35251160a6fb6495a3456c9c1379da95
sherri-ice <sherri.ice7@gmail.com>
2023-02-16 17:38:08 +0300

    Airflow: added load to MongoDB task

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pandas as pd
+import pymongo
 from airflow.decorators import dag, task, task_group
 from airflow.sensors.filesystem import FileSensor
 from airflow.models import Variable
@@ -40,12 +41,20 @@ def tasks_flow():
 
         return remove_unnecessary_symbols(sort_by_created_date(replace_null_values(drop_empty_rows(data))))
 
+    @task
+    def load_to_mongo(data: pd.DataFrame):
+        mongo_db_conn_str = 'mongodb://admin:password@localhost:27017'
+        mongo_client = pymongo.MongoClient(mongo_db_conn_str)
+        db = mongo_client['reviews']  # creates if not exists
+        collection = db['tiktok']
+        collection.insert_many(data.to_dict(orient='records'))
+
     data_path = Variable.get('DATA_PATH')
 
     file_wait_sensor = FileSensor(task_id='waiting_for_data', filepath=data_path)
 
     loaded_csv = file_wait_sensor >> read_csv(data_path)
-    process_data_group(loaded_csv)
+    load_to_mongo(process_data_group(loaded_csv))
 
 
 tasks_flow()
