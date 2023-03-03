bcde80d0a2c5e4cee68a3e5b370208b885af186c
sherri-ice <sherri.ice7@gmail.com>
2023-03-03 14:50:24 +0300

    Airflow: renamed logs schema task

--- alert_project/dags/config_storage/__init__.py
+++ alert_project/dags/config_storage/__init__.py
@@ -9,3 +9,13 @@ class EmailStorage:
     @property
     def email_list(self):
         return self.__email_list
+
+
+class SchemaStorage:
+    def __init__(self, schema_config_path: str):
+        with open(schema_config_path) as json_config:
+            self.__schema_config = json.load(json_config)['schema']
+
+    @property
+    def schema_config(self):
+        return self.__schema_config
--- alert_project/dags/process_mobile_logs_dag.py
+++ alert_project/dags/process_mobile_logs_dag.py
@@ -1,11 +1,12 @@
 import os
 from datetime import datetime, timedelta
+from typing import List
 
 import pandas as pd
 from airflow.decorators import dag, task, task_group
 from airflow.models import Variable
 
-from email_storage import EmailStorage
+from config_storage import EmailStorage, SchemaStorage
 
 default_args = {
     'owner': 'sherri-ice',
@@ -17,12 +18,25 @@ default_args = {
 @dag(dag_id='process_mobile_logs', default_args=default_args, schedule=timedelta(minutes=10),
      start_date=datetime.utcnow(), catchup=False)
 def process_mobile_logs():
-    @task
-    def read_logs_from_csv(log_csv_path: str) -> pd.DataFrame:
-        return pd.read_csv(log_csv_path)
+    @task_group
+    def prepare_logs():
+        @task
+        def read_logs_from_csv(log_csv_path: str) -> pd.DataFrame:
+            return pd.read_csv(log_csv_path)
 
-    data_path = Variable.get('DATA_PATH')
-    read_logs_from_csv(data_path)
+        @task
+        def rename_df_columns(df: pd.DataFrame, new_column_names: List[str]) -> pd.DataFrame:
+            df.columns = new_column_names
+            return df
+
+        data_path = Variable.get('DATA_PATH')
+        schema_config_path = Variable.get('SCHEMA_CONFIG_PATH')
+        new_column_names = SchemaStorage(schema_config_path).schema_config
+
+        data = read_logs_from_csv(data_path)
+        rename_df_columns(data, new_column_names)
+
+    prepare_logs()
 
 
 process_mobile_logs()
